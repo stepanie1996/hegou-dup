a5b7525f21f2986d6b716417269764ef0c74c053
Yeyuxuan1 <130195420+Yeyuxuan1@users.noreply.github.com>
2023-05-17 09:56:17 +0800

    Update config_fuka.js

--- themes/fukasawa/config_fuka.js
+++ themes/fukasawa/config_fuka.js
@@ -7,7 +7,7 @@ const FUKA_CONFIG = {
   MENU_CATEGORY: false, // 显示分类
   MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: false, // 显示归档
-  MENU_SEARCH: false // 显示搜索
+  MENU_SEARCH: true // 显示搜索
 
 }
 export default FUKA_CONFIG
